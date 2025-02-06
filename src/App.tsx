b58cb07d043dd0886b66e1a2efb559b020232b9d
weasl <121301025+chaosweasl@users.noreply.github.com>
2025-02-06 16:57:03 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,43 @@
+import { useState } from "react";
+
 function App() {
+  const [currentCurrency, setCurrentCurrency] = useState("USD");
+  const currencies = ["USD", "EUR", "JPY", "RON"];
+
   return (
     <>
-      <div 
-        id="title"
-        className="text-center m-4">
+      <div id="title" className="text-center m-4">
         <h1>Currency Exchange</h1>
       </div>
 
       <div className="container mt-4">
-      {/* First Input Group with Dropdown */}
-      <div className="input-group mb-2">
-        <button
-          className="btn btn-outline-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Dropdown
-        </button>
-        <ul className="dropdown-menu overflow-auto">
-          <li>
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Separated link
-            </a>
-          </li>
-        </ul>
-        <input type="text" className="form-control" aria-label="Text input with dropdown button" />
+        {/*input group with dropdown box*/}
+        <div className="input-group mb-2">
+          <button
+            className="btn btn-outline-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+          >
+            {currentCurrency}
+          </button>
+          <ul className="dropdown-menu overflow-auto">
+            {currencies.map((currency) => (
+              <li key={currency}>
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={() => setCurrentCurrency(currency)}
+                >
+                  {currency}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <input type="text" className="form-control" />
+        </div>
       </div>
-
-      {/* Second Input Group with Segmented Dropdown */}
-      <div className="input-group">
-        <input type="text" className="form-control" aria-label="Text input with segmented dropdown button" />
-        <button type="button" className="btn btn-outline-secondary">Action</button>
-        <button
-          type="button"
-          className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <span className="visually-hidden">Toggle Dropdown</span>
-        </button>
-        <ul className="dropdown-menu dropdown-menu-end">
-          <li>
-            <a className="dropdown-item" href="#">
-              Action
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Another action
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Something else here
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#">
-              Separated link
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
     </>
-)}
+  );
+}
 
-export default App
+export default App;
