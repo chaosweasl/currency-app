46a21f7e3d449142f1805f85e042aa77797f57d0
weasl <iacobserban17danieloff@gmail.com>
2025-02-08 13:06:06 +0200

    g

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,36 @@ import { useState } from "react";
 
 function App() {
   const [currentCurrency, setCurrentCurrency] = useState("USD");
-  const currencies = ["USD", "EUR", "JPY", "RON"];
+  const currencies = ["USD", "EUR", "JPY", "RON", "RUB"];
+  const currencySymbols = {
+    USD: "$",
+    EUR: "€",
+    JPY: "¥",
+    RON: "lei ",
+    RUB: "₽",
+  };
 
   return (
     <>
+      <div className="d-flex justify-content-center align-items-center my-4">
+        <div
+          className="bg-primary rounded-circle"
+          style={{ width: "2000px", height: "1500px" }}
+        ></div>
+      </div>
+
       <div id="title" className="text-center m-4">
         <h1>Currency Exchange</h1>
       </div>
 
-      <div className="container mt-4">
-        {/*input group with dropdown box*/}
+      {/* <div className="input-group mb-3">
+        <span className="input-group-text">
+          {currencySymbols[currentCurrency]}
+        </span>
+        <input type="text" className="form-control" placeholder="Username" />
+      </div>
+
+      <div className="container d-flex flex-column justify-content-center mt-4 w-50">
         <div className="input-group mb-2">
           <button
             className="btn btn-outline-secondary dropdown-toggle"
@@ -33,9 +53,13 @@ function App() {
               </li>
             ))}
           </ul>
-          <input type="text" className="form-control" />
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Enter the amount here..."
+          />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
